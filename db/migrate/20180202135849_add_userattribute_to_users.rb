8883f83b49a212664fc61d535117593cc111fd1e
bambi1859 <hoyein9297@likelion.org>
2018-02-02 23:28:52 +0900

    first

---
+++ db/migrate/20180202135849_add_userattribute_to_users.rb
@@ -0,0 +1,8 @@
+class AddUserattributeToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :userage, :integer
+    add_column :users, :usersex, :string
+    add_column :users, :admin, :boolean
+  end
+end
